b27b4b0db57ebde3715886a2f8040ea808677113
ukseongchoi <sunguk132@naver.com>
2024-02-27 13:48:42 +0900

    0227

--- sound.py
+++ sound.py
@@ -1,3 +1,13 @@
-import playsound
+import pygame
+from openai import OpenAI
 
-playsound.playsound("t_time.mp3")
+filename = 's_key.txt'
+f = open(filename, 'r')
+OPENAI_API_KEY = f
+
+#client = OpenAI(api_key = OPENAI_API_KEY)
+
+
+pygame.mixer.init()
+bang =  pygame.mixer.Sound("t_time")
+bang.play()
