9df02a03e80388bf5b03d8968d386a697ad54f65
ukseongchoi <sunguk132@naver.com>
2024-03-04 03:06:56 +0900

    0327

--- my_ai.py
+++ my_ai.py
@@ -9,3 +9,25 @@ sp = gTTS(
     slow=False
 )
 sp.save(audio)
+
+
+
+import speech_recognition
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+mic = sr.Microphone()
+with mic as source:
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source, timeout = 5, phrase_time_limit = 5)
+
+try:
+    result = r.recognize_google(audio, language = "ko-KR")
+    print(result)
+except speech_recognition.UnknownValueError:
+    print("음성 인식 실패")
+except speech_recognition.RequestError:
+    print("HTTP Request Error 발생")
+except speech_recognition.WaitTimeoutError:
+    print("WaitTimeout Error 발생 ㅠㅠ")
