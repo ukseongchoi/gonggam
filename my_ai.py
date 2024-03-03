9beb980f4137ea03142bb1493fd3a14d95ae1975
ukseongchoi <sunguk132@naver.com>
2024-03-04 04:30:49 +0900

    0430

--- my_ai.py
+++ my_ai.py
@@ -8,44 +8,37 @@ import speech_recognition as sr
 
 import pygame
 
+while True:
 
+    try:
+        r = sr.Recognizer()
+        mic = sr.Microphone()
+        with mic as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source, timeout = 5, phrase_time_limit = 5)
 
-
-def call_ai():
-    stt = ''
-
-    while True:
-        if stt.replace(" ",'') == "헤이공감":
-
+    
+        result = r.recognize_google(audio, language = "ko-KR")
+        stt += result
+        print(stt)
+        if '헤이공감' in stt.replace(' ',''):
+              
             sp = gTTS(
-            lang='ko',
-            text='네 안녕하세요',
-            slow=False
-            )
+                lang='ko',
+                text='안녕하세요',
+                slow=False
+                )
             sp.save(file_name)
 
+            
             pygame.mixer.init()
             tts = pygame.mixer.Sound(file_name)
             tts.play()
 
             os.remove(file_name)
 
-            break
-
-
-        try:
-            r = sr.Recognizer()
-            mic = sr.Microphone()
-            with mic as source:
-                r.adjust_for_ambient_noise(source)
-                audio = r.listen(source, timeout = 5, phrase_time_limit = 5)
-
-        
-            result = r.recognize_google(audio, language = "ko-KR")
-            stt += result
-
-        except Exception as e:
+    except Exception as e:
             print("Exception: " + str(e))
 
 
-call_ai()
+
