46745cc60e0a635e6146de23b0641f51b32d7e37
ukseongchoi <sunguk132@naver.com>
2024-03-04 03:27:27 +0900

    0327

--- my_ai.py
+++ my_ai.py
@@ -5,15 +5,18 @@ import speech_recognition as sr
 audio = 'speech.mp3'
 
 
-r = sr.Recognizer()
-mic = sr.Microphone()
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source, timeout = 5, phrase_time_limit = 5)
+
 
 stt = ''
 
 while True:
+
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, timeout = 5, phrase_time_limit = 5)
+
     try:
         result = r.recognize_google(audio, language = "ko-KR")
         stt = result
