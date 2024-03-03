81c8b6084014778eca08a9465cab3ccc8cb1083a
ukseongchoi <sunguk132@naver.com>
2024-03-04 03:12:37 +0900

    0312

--- my_ai.py
+++ my_ai.py
@@ -1,33 +1,33 @@
 from gtts import gTTS
+import speech_recognition as sr
 
 audio = 'speech.mp3'
-language = 'ko'
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+stt = ''
+while True:
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, timeout = 5, phrase_time_limit = 5)
+
+    try:
+        result = r.recognize_google(audio, language = "ko-KR")
+        stt = result
+        print(stt)
+    except speech_recognition.UnknownValueError:
+        print("음성 인식 실패")
+    except speech_recognition.RequestError:
+        print("HTTP Request Error")
+    except speech_recognition.WaitTimeoutError:
+        print("WaitTimeout Error")
+
+
 
 sp = gTTS(
-    lang=language,
+    lang='ko',
     text="공감수학학원",
     slow=False
 )
 sp.save(audio)
-
-
-
-import speech_recognition
-import speech_recognition as sr
-
-r = sr.Recognizer()
-
-mic = sr.Microphone()
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source, timeout = 5, phrase_time_limit = 5)
-
-try:
-    result = r.recognize_google(audio, language = "ko-KR")
-    print(result)
-except speech_recognition.UnknownValueError:
-    print("음성 인식 실패")
-except speech_recognition.RequestError:
-    print("HTTP Request Error 발생")
-except speech_recognition.WaitTimeoutError:
-    print("WaitTimeout Error 발생 ㅠㅠ")
