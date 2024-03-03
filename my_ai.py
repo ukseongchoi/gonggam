2e0c40e4b0fd80d09f72db5c97f7cd2d9659a993
ukseongchoi <sunguk132@naver.com>
2024-03-04 04:39:53 +0900

    0439

--- my_ai.py
+++ my_ai.py
@@ -8,6 +8,20 @@ import speech_recognition as sr
 
 import pygame
 
+
+sp = gTTS(
+    lang='ko',
+    text='네 안녕하세요',
+    slow=False
+    )
+sp.save('yes.mp3')
+
+        
+#pygame.mixer.init()
+#tts = pygame.mixer.Sound(file_name)
+#tts.play()
+
+
 while True:
 
     try:
@@ -17,24 +31,10 @@ while True:
             r.adjust_for_ambient_noise(source)
             audio = r.listen(source, timeout = 5, phrase_time_limit = 5)
 
-    
         result = r.recognize_google(audio, language = "ko-KR")
         stt += result
         print(stt)
         
-        sp = gTTS(
-            lang='ko',
-            text='안녕하세요',
-            slow=False
-            )
-        sp.save(file_name)
-
-        
-        pygame.mixer.init()
-        tts = pygame.mixer.Sound(file_name)
-        tts.play()
-
-        os.remove(file_name)
 
     except Exception as e:
             print("Exception: " + str(e))
